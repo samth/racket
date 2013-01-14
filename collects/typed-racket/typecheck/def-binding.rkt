f438b91e3584d4ae65b97587d75c2b046a500d9c
Sam Tobin-Hochstadt <samth@racket-lang.org>
2013-01-14 12:06:45 -0500

    Use `cond-contract`.

--- collects/typed-racket/typecheck/def-binding.rkt
+++ collects/typed-racket/typecheck/def-binding.rkt
@@ -1,18 +1,17 @@
 #lang racket/base
 
-(require racket/contract "../utils/utils.rkt" racket/struct-info)
+(require "../utils/utils.rkt" racket/struct-info)
 
 (define-struct binding (name) #:transparent)
 (define-struct (def-binding binding) (ty) #:transparent)
 (define-struct (def-stx-binding binding) () #:transparent)
 (define-struct (def-struct-stx-binding def-stx-binding) (static-info constructor-type) #:transparent)
 
-(provide
-  (contract-out
-    (struct binding ([name identifier?]))
-    (struct (def-binding binding) ([name identifier?] [ty any/c]))
-    (struct (def-stx-binding binding) ([name identifier?]))
-    (struct (def-struct-stx-binding def-stx-binding)
-            ([name identifier?]
-             [static-info (or/c #f struct-info?)]
-             [constructor-type any/c]))))
+(provide/cond-contract
+ (struct binding ([name identifier?]))
+ (struct (def-binding binding) ([name identifier?] [ty any/c]))
+ (struct (def-stx-binding binding) ([name identifier?]))
+ (struct (def-struct-stx-binding def-stx-binding)
+         ([name identifier?]
+          [static-info (or/c #f struct-info?)]
+          [constructor-type any/c])))

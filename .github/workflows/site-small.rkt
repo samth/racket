59e63d677d5851065e3d3bc19c9d34e0a20c63bb
Sam Tobin-Hochstadt <samth@indiana.edu>
2020-04-28 11:26:43 -0400

    more work.

---
+++ .github/workflows/site-small.rkt
@@ -0,0 +1,34 @@
+#lang distro-build/config
+(require racket/format)
+
+(define (build-dir-name)
+  (case (current-mode)
+    [("release") "release-build"]
+    [else "build"]))
+(define (dest-dir-name)
+  (case (current-mode)
+    [("release") "ci-release"]
+    [else (~a "ci-snapshots/" (current-stamp))]))
+
+(define server-base-url (~a "https://snapshots.racket-lang.org/" (dest-dir-name) "/"))
+
+(define distro-content
+  '("racket-lib" "base" "compiler-lib"))
+
+
+;; The overall configuration:
+(parallel
+ #:pkgs distro-content
+ #:dist-base-url server-base-url
+ #:site-dest (build-path (getenv "DISTRO_BUILD_SITE_DEST") (dest-dir-name))
+ ;#:dir (~a (build-dir-name) "/plt")
+ #:plt-web-style? #t
+ #:site-title (format "Snapshot: ~a" (current-stamp))
+ (machine #:name "Racket_BC"
+          ;#:racket "/usr/bin/racket"
+          #:versionless? #true)
+ #;(machine #:name "Racket_CS"
+          ;#:racket "/usr/bin/racket"
+          #:versionless? #true
+          #:variant 'cs
+          #:dist-suffix "cs"))

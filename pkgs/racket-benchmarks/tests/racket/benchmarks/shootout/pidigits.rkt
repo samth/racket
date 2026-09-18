4675e358ec0c90996865888746f715b45e340372
Sam Tobin-Hochstadt <samth@iu.edu>
2026-09-18 15:36:37 -0400

    Improve FASTA state locality and specialize exact pidigits arithmetic

--- pkgs/racket-benchmarks/tests/racket/benchmarks/shootout/pidigits.rkt
+++ pkgs/racket-benchmarks/tests/racket/benchmarks/shootout/pidigits.rkt
@@ -7,39 +7,31 @@
 
 (require racket/cmdline)
 
-(define (floor_ev q r s t x)
-  (quotient (+ (* q x) r) (+ (* s x) t)))
-
-(define (comp q r s t  q2 r2 s2 t2)
-  (values (+ (* q q2) (* r s2))
-          (+ (* q r2) (* r t2))
-          (+ (* s q2) (* t s2))
-          (+ (* s r2) (* t t2))))
-
-(define (next q r s t) (floor_ev q r s t 3))
-(define (safe? q r s t n) (= n (floor_ev q r s t 4)))
-(define (prod q r s t n) (comp 10 (* -10 n) 0 1  q r s t))
-(define (mk q r s t k) (comp q r s t k (* 2 (add1 (* 2 k))) 0 (add1 (* 2 k))))
-
-(define (digit k  q r s t  n row col)
+;; The LFT's lower-left coefficient stays zero. Keep both candidate
+;; extractions and use exact shifts for the power-of-two multipliers.
+(define (digit k q r t n row col)
   (if (> n 0)
-      (let ([y (next q r s t)])
-        (if (safe? q r s t y)
-            (let-values ([(q r s t) (prod q r s t y)])
+      (let ([y (quotient (+ (* q 3) r) t)])
+        (if (= y (quotient (+ (arithmetic-shift q 2) r) t))
+            (let ([q (* 10 q)] [r (* 10 (- r (* y t)))])
               (if (= col 10)
                   (let ([row (+ row 10)])
                     (printf "\t:~a\n~a" row y)
-                    (digit k q r s t (sub1 n) row 1))
+                    (digit k q r t (sub1 n) row 1))
                   (begin
                     (printf "~a" y)
-                    (digit k q r s t (sub1 n) row (add1 col)))))
-            (let-values ([(q r s t) (mk q r s t k)])
-              (digit (add1 k) q r s t n row col))))
+                    (digit k q r t (sub1 n) row (add1 col)))))
+            (let ([k2 (add1 (* 2 k))])
+              (digit (add1 k)
+                     (* q k)
+                     (* (+ r (arithmetic-shift q 1)) k2)
+                     (* t k2)
+                     n row col))))
       (printf "~a\t:~a\n"
               (make-string (- 10 col) #\space)
               (+ row col))))
 
 (define (digits n)
-  (digit 1  1 0 0 1  n 0 0))
+  (digit 1 1 0 1 n 0 0))
 
 (digits (command-line #:args (n) (string->number n)))
